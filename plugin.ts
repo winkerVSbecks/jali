fde0619a0006bac75765c90ff31e684aaccdc32d
winkerVSbecks <varunvachhar@gmail.com>
2020-04-10 15:04:49 -0400

    more tile types

--- plugin.ts
+++ plugin.ts
@@ -1,22 +1,23 @@
 import { createPath, pathsToPolylines } from 'canvas-sketch-util/penplot';
 import Random from 'canvas-sketch-util/random';
 
-figma.showUI(__html__, { width: 300, height: 240 })
+figma.showUI(__html__, { width: 300, height: 260 });
 
 figma.ui.onmessage = msg => {
   if (msg.type === 'create-jali') {
-    main(msg.count, msg.resolution, msg.stroke).then(() => {
-      figma.closePlugin();
-    }, (error) => {
-      figma.ui.postMessage({ type: 'error', value: error });
-    });
+    main(msg.count, msg.resolution, msg.stroke, msg.tileType).then(
+      () => { },
+      (error) => {
+        figma.ui.postMessage({ type: 'error', value: error });
+      });
   } else {
     figma.closePlugin();
   }
 }
 
+type TileType = 'crossOverArcs' | 'arcs' | 'diagonals' | 'diagonalMesh' | 'overlappingArcs' | 'arcSweeps';
 
-function main(curveCount: number = 2, resolution: number = 10, strokeWeight: number = 4): Promise<string | undefined> {
+function main(curveCount: number = 2, resolution: number = 10, strokeWeight: number = 4, tileType: TileType): Promise<string | undefined> {
   // Make sure the selection is a single piece of text before proceeding.
   if (figma.currentPage.selection.length !== 1) {
     return Promise.reject("error: select just one frame");
@@ -40,9 +41,18 @@ function main(curveCount: number = 2, resolution: number = 10, strokeWeight: num
   const paths: any[] = [];
   const s = width / resolution;
 
+  const tileTypes = {
+    crossOverArcs: crossOverArcs,
+    arcs: arcs,
+    diagonals: diagonals,
+    diagonalMesh: diagonalMesh,
+    overlappingArcs: overlappingArcs,
+    arcSweeps: arcSweeps(curveCount),
+  };
+
   for (let x = 0; x < width; x += s) {
     for (let y = 0; y < height; y += s) {
-      const tile = Random.pick(arcSweeps(curveCount));
+      const tile = Random.pick(tileTypes[tileType]);
       paths.push(tile([x, y], s));
     }
   }
@@ -98,6 +108,167 @@ function renderPath(pts, strokeWeight: number): VectorNode {
 
   return node;
 }
+
+const crossOverArcs = [
+  function ([x, y], s) {
+    const p = createPath();
+
+    p.moveTo(x + s / 2, y);
+    p.lineTo(x + s / 2, y + s / 4);
+
+    p.moveTo(x + s / 2, y + s / 2 + s / 4);
+    p.lineTo(x + s / 2, y + s);
+
+    p.moveTo(x, y + s / 2);
+    p.lineTo(x + s, y + s / 2);
+
+    return p;
+  },
+  function ([x, y], s) {
+    const p = createPath();
+
+    p.arc(x, y, s / 2, 0, Math.PI / 2);
+
+    p.moveTo(x + s / 2, y + s);
+    p.arc(x + s, y + s, s / 2, Math.PI, (3 * Math.PI) / 2);
+
+    return p;
+  },
+  function ([x, y], s) {
+    const p = createPath();
+
+    p.arc(x, y + s, s / 2, (3 * Math.PI) / 2, 0);
+
+    p.moveTo(x + s, y + s / 2);
+    p.arc(x + s, y, s / 2, Math.PI / 2, Math.PI);
+
+    return p;
+  },
+];
+
+const arcs = [
+  function ([x, y], s) {
+    const p = createPath();
+    return p;
+  },
+  function ([x, y], s) {
+    const p = createPath();
+    p.arc(x, y, s / 2, 0, Math.PI / 2);
+    return p;
+  },
+  function ([x, y], s) {
+    const p = createPath();
+    p.arc(x + s, y + s, s / 2, Math.PI, (3 * Math.PI) / 2);
+    return p;
+  },
+  function ([x, y], s) {
+    const p = createPath();
+    p.arc(x, y + s, s / 2, (3 * Math.PI) / 2, 0);
+    return p;
+  },
+  function ([x, y], s) {
+    const p = createPath();
+    p.arc(x + s, y, s / 2, Math.PI / 2, Math.PI);
+    return p;
+  },
+  function ([x, y], s) {
+    const p = createPath();
+    p.arc(x, y, s / 2, 0, Math.PI / 2);
+    p.arc(x, y + s, s / 2, (3 * Math.PI) / 2, 0);
+    p.arc(x + s, y + s, s / 2, Math.PI, (3 * Math.PI) / 2);
+    p.arc(x + s, y, s / 2, Math.PI / 2, Math.PI);
+    return p;
+  },
+];
+
+const diagonals = [
+  function ([x, y], s) {
+    const p = createPath();
+    p.moveTo(x + s / 2, y);
+    p.lineTo(x, y + s / 2);
+    return p;
+  },
+  function ([x, y], s) {
+    const p = createPath();
+    p.moveTo(x + s / 2, y);
+    p.lineTo(x + s, y + s / 2);
+    return p;
+  },
+  function ([x, y], s) {
+    const p = createPath();
+    p.moveTo(x, y + s / 2);
+    p.lineTo(x + s / 2, y + s);
+    return p;
+  },
+  function ([x, y], s) {
+    const p = createPath();
+    p.moveTo(x + s / 2, y + s);
+    p.lineTo(x + s, y + s / 2);
+    return p;
+  },
+];
+
+const diagonalMesh = [
+  function ([x, y], s) {
+    const p = createPath();
+    p.moveTo(x + s / 2, y);
+    p.lineTo(x, y + s / 2);
+    p.moveTo(x + s / 2, y + s);
+    p.lineTo(x + s, y + s / 2);
+    return p;
+  },
+  function ([x, y], s) {
+    const p = createPath();
+    p.moveTo(x, y + s / 2);
+    p.lineTo(x + s / 2, y + s);
+    p.moveTo(x + s / 2, y);
+    p.lineTo(x + s, y + s / 2);
+    return p;
+  },
+  function ([x, y], s) {
+    const p = createPath();
+    p.moveTo(x + s / 2, y);
+    p.lineTo(x + s / 2, y + s);
+    p.moveTo(x, y + s / 2);
+    p.lineTo(x + s, y + s / 2);
+    return p;
+  },
+];
+
+const overlappingArcs = [
+  function ([x, y], s) {
+    const r = s / 4;
+    const p = createPath();
+    p.arc(x, y, r, 0, Math.PI / 2);
+    p.moveTo(x + 2 * r, y);
+    p.arc(x, y, r * 2, 0, Math.PI / 2);
+    p.moveTo(x + 3 * r, y);
+    p.arc(x, y, r * 3, 0, Math.PI / 2);
+    p.moveTo(x + s - r, y + s);
+    p.arc(x + s, y + s, r, Math.PI, (3 * Math.PI) / 2);
+    p.moveTo(x + s - 2 * r, y + s);
+    p.arc(x + s, y + s, r * 2, Math.PI, (3 * Math.PI) / 2);
+    p.moveTo(x + s - 3 * r, y + s);
+    p.arc(x + s, y + s, r * 3, Math.PI, (3 * Math.PI) / 2);
+    return p;
+  },
+  function ([x, y], s) {
+    const r = s / 4;
+    const p = createPath();
+    p.arc(x + s, y, r, Math.PI / 2, Math.PI);
+    p.moveTo(x + s, y + 2 * r);
+    p.arc(x + s, y, r * 2, Math.PI / 2, Math.PI);
+    p.moveTo(x + s, y + 3 * r);
+    p.arc(x + s, y, r * 3, Math.PI / 2, Math.PI);
+    p.moveTo(x + r, y + s);
+    p.arc(x, y + s, r, (3 * Math.PI) / 2, 2 * Math.PI);
+    p.moveTo(x + 2 * r, y + s);
+    p.arc(x, y + s, r * 2, (3 * Math.PI) / 2, 2 * Math.PI);
+    p.moveTo(x + 3 * r, y + s);
+    p.arc(x, y + s, r * 3, (3 * Math.PI) / 2, 2 * Math.PI);
+    return p;
+  },
+];
 
 const arcSweeps = (count) => [
   rail(count, 'H'),
--- ui.ts
+++ ui.ts
@@ -3,12 +3,14 @@ document.getElementById('create')!.onclick = () => {
   const countTextbox = document.getElementById('count')! as HTMLInputElement;
   const resolutionTextbox = document.getElementById('resolution')! as HTMLInputElement;
   const strokeTextbox = document.getElementById('stroke')! as HTMLInputElement;
+  const tileTypeSelect = document.getElementById('tile-type')! as HTMLInputElement;
 
   const count = parseInt(countTextbox.value, 10);
   const resolution = parseInt(resolutionTextbox.value, 10);
   const stroke = parseInt(strokeTextbox.value, 10);
+  const tileType = tileTypeSelect.value;
 
-  parent.postMessage({ pluginMessage: { type: 'create-jali', count, resolution, stroke } }, '*');
+  parent.postMessage({ pluginMessage: { type: 'create-jali', count, resolution, stroke, tileType } }, '*');
 };
 
 onmessage = (event) => {
